4979feb91ccce4c551a82cb4aba6d0a14f150cbd
Sergey <sergey.sera@yandex.ru>
2024-06-20 12:15:40 +0300

    Updated route-switcher with faster checking time

---
+++ examples/route-switcher.tf
@@ -0,0 +1,36 @@
+module "route_switcher" {
+  source    = "../route-switcher/"
+  start_module          = true
+  folder_id = var.folder_id
+  route_table_folder_list = [var.folder_id]
+  route_table_list      = [yandex_vpc_route_table.nat_instance_rt.id]
+  router_healthcheck_port = 22
+  back_to_primary = true
+  router_healthcheck_interval = 60
+  routers = [
+    {
+      # nat-a
+      healthchecked_ip = yandex_compute_instance.nat_a.network_interface.0.ip_address
+      healthchecked_subnet_id = yandex_vpc_subnet.public_subnet_a.id
+      interfaces = [
+        {
+          # private-int
+          own_ip = yandex_compute_instance.nat_a.network_interface.0.ip_address
+          backup_peer_ip = yandex_compute_instance.nat_b.network_interface.0.ip_address
+        }
+      ]
+    },
+    {
+      # nat-b
+      healthchecked_ip = yandex_compute_instance.nat_b.network_interface.0.ip_address
+      healthchecked_subnet_id = yandex_vpc_subnet.public_subnet_b.id
+      interfaces = [
+        {
+          # private-int
+          own_ip = yandex_compute_instance.nat_b.network_interface.0.ip_address
+          backup_peer_ip = yandex_compute_instance.nat_a.network_interface.0.ip_address
+        }
+      ]
+    }
+  ]
+}
